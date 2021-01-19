20e528ccf2799a324fd3160b4eddeee45a6647d2
Kang <2881570+kangni@users.noreply.github.com>
2021-01-19 21:34:14 +0800

    update method

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,17 +4,46 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-    println!("rect1 is {:#?}", rect1);
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 20,
+        height: 20,
+    };
+
+    let rect3 = Rectangle {
+        width: 90,
+        height: 90,
+    };
+
+    println!(
+        "The area of the rectangle 1 is {} square pixels.",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("Square rectangle: {:#?}", Rectangle::square(5));
 }
